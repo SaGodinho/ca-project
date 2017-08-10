7fbdb0654308aab869fee3a64bd20685ab8de2da
John Doe <nothing@nowhere.com>
2017-08-10 14:32:41 +0200

    Clean up container before running

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 docker stop ca-app
+# You may want to do this differently if you want to keep or investigate stopped containers
+docker rm ca-app
 docker run -d -p 8000:5000 --name ca-app ca-app
